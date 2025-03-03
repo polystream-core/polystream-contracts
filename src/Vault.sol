a01c2558392972b733c9059319e3a7097b3a46f8
gabr1234iel <gabrrrly@gmail.com>
2025-03-03 18:13:38 +0800

    arrange test

--- src/Vault.sol
+++ src/Vault.sol
@@ -1,28 +1,28 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.19;
 
-//IMPORTS
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/utils/math/Math.sol";
+// //IMPORTS
+// import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+// import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
+// import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+// import "@openzeppelin/contracts/utils/math/Math.sol";
 
-//INTERFACES
+// //INTERFACES
 
 
 
-contract YieldVault is ERC4626, Ownable, ReentrancyGuard {
-    using SafeERC20 for IERC20;
-    using Math for uint256;
+// contract YieldVault is ERC4626, Ownable, ReentrancyGuard {
+//     using SafeERC20 for IERC20;
+//     using Math for uint256;
 
-    //STATE VARIABLES
-    
+//     //STATE VARIABLES
 
-    //CONSTRUCTORS
 
-    //CORE FUNCTIONS
+//     //CONSTRUCTORS
 
-    //VIEW FUNCTIONS
+//     //CORE FUNCTIONS
 
-}
+//     //VIEW FUNCTIONS
+
+// }
